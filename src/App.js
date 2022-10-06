4ddcac22d1e8b2a13cf79a3fd4cdfe0ae4315dc7
chenjunqian <chenjunqian0810@foxmail.com>
2022-10-06 21:02:51 +0800

    add theme and header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './views/Home';
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { orange, purple } from "@mui/material/colors";
 
 export const AppContext = React.createContext(null);
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: orange[800],
+    },
+    secondary: {
+      main: purple[500],
+    },
+  },
+});
 
 function App() {
 
@@ -10,11 +23,13 @@ function App() {
 
   return (
     <AppContext.Provider value={appContext}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/*' element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/*' element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </AppContext.Provider>
   );
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,121 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { AppBar, IconButton, Toolbar, Box, Divider, List, ListItemButton, ListItemIcon, ListItemText, Drawer, CssBaseline, Typography } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import { useNavigate } from "react-router-dom";
 
 
-function HomePage() {
+const drawerWidth = 240;
+
+function Home(props) {
+
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const navigate = useNavigate()
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+
+  const handlerListItemClick = (menuType) => {
+    setMobileOpen(!mobileOpen)
+    switch (menuType) {
+      case "tg-video":
+        navigate("/timeline")
+        break
+
+      default:
+        break
+    }
+  }
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        <ListItemButton onClick={() => { handlerListItemClick("timeline") }}>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Timeline" />
+        </ListItemButton>
+      </List>
+    </div>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <div>
-      This is home page
-    </div>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={handleDrawerToggle}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            GuoShao
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+      >
+        <Toolbar />
+        <Routes>
+          <Route path="/timeline" element={<div />} />
+        </Routes>
+
+      </Box>
+    </Box>
   )
 }
-
-export default HomePage;
+export default Home;
