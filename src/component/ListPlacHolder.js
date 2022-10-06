cf021651b58cbde8857c6d98256de39ec7699bf7
chenjunqian <chenjunqian0810@foxmail.com>
2022-10-07 00:05:27 +0800

    add place holder for list:

---
+++ src/component/ListPlacHolder.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Skeleton
+} from "@mui/material";
+
+
+const ListLoadingPlaceholder = () => {
+
+  const ItemPlaceHolder = (
+    <Card style={{ width: '94%', marginLeft: '2%', marginRight: '2%', marginBottom: '8px' }}>
+      <CardActionArea>
+        <CardHeader
+          avatar={
+            <Skeleton animation="wave" variant="circle" width={40} height={40} />
+          }
+          title={
+            <Skeleton animation="wave" height={10} width="80%" style={{ marginBottom: 6 }} />
+          }
+          subheader={
+            <Skeleton animation="wave" height={10} width="40%" />
+          }
+        />
+        <CardContent>
+          <Skeleton style={{ height: 190 }} animation="wave" variant="rect" />
+          <React.Fragment>
+            <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
+            <Skeleton animation="wave" height={10} width="80%" />
+          </React.Fragment>
+        </CardContent>
+      </CardActionArea>
+      <CardActions disableSpacing>
+
+      </CardActions>
+    </Card>
+
+  )
+
+  return (
+    <div>
+      <ItemPlaceHolder />
+      <ItemPlaceHolder />
+      <ItemPlaceHolder />
+    </div>
+  )
+}
+
+export default ListLoadingPlaceholder;
