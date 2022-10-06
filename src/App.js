9f39f45442e99301a36657063f7e7d345e81b678
chenjunqian <chenjunqian0810@foxmail.com>
2022-10-06 20:44:40 +0800

    add home pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './views/Home';
+
+export const AppContext = React.createContext(null);
 
 function App() {
-  return (
-    <div >
 
-    </div>
+  const appContext = {}
+
+  return (
+    <AppContext.Provider value={appContext}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function HomePage() {
+
+  return (
+    <div>
+      This is home page
+    </div>
+  )
+}
+
+export default HomePage;
