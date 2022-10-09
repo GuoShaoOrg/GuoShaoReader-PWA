138eda87cdf26fa1bb9664fcf2af80cb72d652cb
chenjunqian <chenjunqian0810@foxmail.com>
2022-10-10 00:53:17 +0800

    add empty view

--- src/component/CommonFeedList.js
+++ src/component/CommonFeedList.js
@@ -4,14 +4,16 @@ import CommonFeedListItem from "./CommonFeedListItem";
 import Pagination from "@mui/material/Pagination"
 import Box from "@mui/material/Box"
 import ListLoadingPlaceholder from "./ListPlacHolder";
+import EmptyView from "./EmptyView";
 
 
 function CommonFeedList(props) {
 
   const fetchData = props.fetchData
-  const [feedList, setFeedList] = React.useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+  const [feedList, setFeedList] = React.useState([])
   const [loading, setLoading] = React.useState(true);
   const [page, setPage] = React.useState(1)
+  const [subFeedListEmpty, setSubFeedListEmpty] = React.useState(false)
 
   const handlePaginationChange = (_event, value) => {
     setPage(value)
@@ -33,29 +35,37 @@ function CommonFeedList(props) {
         setLoading(false)
         return
       }
-      setLoading(false)
       setFeedList(resp)
 
     })
+    setLoading(false)
+    if (feedList.length === 0) {
+      console.log("feed list is empty")
+      setSubFeedListEmpty(true)
+    }
   }, [])
 
 
 
   return (
     <div className="w-full">
-      <Box className="flex justify-center">
-        {loading ? (<ListLoadingPlaceholder />) :
-          <List sx={{ width: '100%', maxWidth: 720 }} className="w-full">
-            {feedList.map((item, index) => (
-              <ListItem key={index} disablePadding className="mt-4">
-                <CommonFeedListItem />
-              </ListItem>
-            ))}
-          </List>}
-      </Box>
-      <Box display="flex" justifyContent="center">
-        <Pagination className="place-self-center" count={10} page={page} color="primary" onChange={handlePaginationChange} />
-      </Box>
+      {subFeedListEmpty ? (<EmptyView />) :
+        <div>
+          <Box className="flex justify-center">
+            {loading ? (<ListLoadingPlaceholder />) :
+              <List sx={{ width: '100%', maxWidth: 720 }} className="w-full">
+                {feedList.map((item, index) => (
+                  <ListItem key={index} disablePadding className="mt-4">
+                    <CommonFeedListItem data={item} />
+                  </ListItem>
+                ))}
+              </List>}
+          </Box>
+          <Box display="flex" justifyContent="center">
+            <Pagination className="place-self-center" count={10} page={page} color="primary" onChange={handlePaginationChange} />
+          </Box>
+        </div>
+      }
     </div>
   )
 }
---
+++ src/component/EmptyView.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function EmptyView() {
+
+  return (
+    <div className="place-self-center">empty view</div>
+  )
+}
+
+
+export default EmptyView;
