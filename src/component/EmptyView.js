553c5f674ff450628b1a99ce0166767f2f583270
chenjunqian <chenjunqian0810@foxmail.com>
2022-10-12 00:14:13 +0800

    add lottie and empty animation

--- src/component/EmptyView.js
+++ src/component/EmptyView.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Typography from '@mui/material/Typography';
+import EmptyAnimation from "../assets/empty.json"
+import Lottie from "lottie-react"
 
 
 function EmptyView() {
 
   return (
-    <div className="place-self-center">empty view</div>
+    <div className="w-full max-h-96">
+      <div className="flex justify-center mt-24">
+        <Lottie animationData={EmptyAnimation} className="md:w-1/4 w-1/3" />
+      </div>
+      <div className="flex justify-center">
+        <Typography variant="body1" gutterBottom>
+          还没有订阅源，赶快去添加吧
+        </Typography>
+      </div>
+    </div>
   )
 }
 
