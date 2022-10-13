10713875feb61152c2bd09d56881be5a54707c8e
chenjunqian <chenjunqian0810@foxmail.com>
2022-10-14 00:04:18 +0800

    refine login page style

--- src/views/LoginRegister.js
+++ src/views/LoginRegister.js
@@ -38,7 +38,7 @@ function LoginRegister() {
 
   return (
     <div className="w-full">
-      <div className="flex justify-center md:-mt-32 sm:-mt-28 -mt-32">
+      <div className="flex justify-center md:-mt-32 sm:-mt-28 lg:-mt-48 -mt-32">
         <Lottie animationData={LoginAnimation} loop={true} className="md:w-1/2 w-4/5 max-w-md" />
       </div>
       <div className="w-full flex justify-center">
@@ -55,9 +55,7 @@ function LoginRegister() {
           <TabPanel value={tabValue} index={1}>
             <RegisterView />
           </TabPanel>
-
         </div>
-
       </div>
     </div>
   )
