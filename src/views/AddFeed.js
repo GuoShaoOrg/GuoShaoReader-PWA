139413886e4c592531a661b8a3bab54d003bb759
chenjunqian <chenjunqian0810@foxmail.com>
2022-10-19 00:10:23 +0800

    add url animation for add feed page

--- src/views/AddFeed.js
+++ src/views/AddFeed.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Button, TextField } from "@mui/material";
 import { AppContext } from "../App";
 import Toast from "../component/Toast";
+import Lottie from "lottie-react"
+import UrlAnimation from "../assets/lotties/url.json"
+
 
 function AddFeed() {
 
@@ -20,9 +23,10 @@ function AddFeed() {
 
   return (
     <div className="w-full">
-      <div className="flex justify-center">
+      <div className="flex justify-center -mt-24">
+        <Lottie animationData={UrlAnimation} loop={true} className="md:w-1/2 w-4/5 max-w-md" />
       </div>
-      <div className="flex justify-center h-12">
+      <div className="flex justify-center h-12 -mt-32">
         <TextField className="md:w-1/2 w-4/5 max-w-xs" onChange={handleFeedInputChange} id="input-with-sx" label="订阅源" variant="standard" />
       </div>
       <div className="flex justify-center h-12 mt-12">
