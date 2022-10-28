f32582cc272782d79367d7a07efe26d5f6d2aaa8
chenjunqian <chenjunqian0810@foxmail.com>
2022-10-29 00:41:54 +0800

    add title

--- src/views/AccountPage.js
+++ src/views/AccountPage.js
@@ -18,6 +18,9 @@ function AccountPage() {
   return (
     <div className="w-full flex justify-center mt-10 mx-5">
       <div className="max-w-3xl w-4/5 py-10 rounded-lg border border-solid ">
+        <div className="mx-5">
+          <h3 className="text-lg font-medium leading-6 text-gray-900 mx-4 mb-4">账户信息</h3>
+        </div>
         <div className="bg-gray-50 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt className="text-sm font-medium text-gray-500 mx-4">昵称</dt>
           <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 mx-4">{userInfo.username}</dd>
